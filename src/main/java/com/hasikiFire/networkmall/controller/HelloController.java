ca4f8f5e82bb54ebeb12bafdf273c7279125e9d7
hasikiFire <gedune@outlook.com>
2024-05-28 23:59:39 +0800

    fix: vscode

--- src/main/java/com/hasikiFire/networkmall/controller/HelloController.java
+++ src/main/java/com/hasikiFire/networkmall/controller/HelloController.java
@@ -6,9 +6,8 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMapping;
 
- 
 @RestController
 public class HelloController {
 
@@ -17,16 +16,16 @@ public class HelloController {
         return "Greetings from Spring Boot!";
     }
 
-  @RequestMapping("/helloworld")
-  public String helloWorld() {
-      return "helloworld";
-  }
+    @RequestMapping("/helloworld")
+    public String helloWorld() {
+        return "helloworld";
+    }
 
-  @RequestMapping("/user")
-  public Map getUsers() {
-      Map map = new HashMap<>();
-      map.put("name","fangxiao");
-      map.put("age","15");
-      return map;
-  }
+    @RequestMapping("/user")
+    public Map getUsers() {
+        Map map = new HashMap<>();
+        map.put("name", "fangxiao");
+        map.put("age", "15");
+        return map;
+    }
 }
